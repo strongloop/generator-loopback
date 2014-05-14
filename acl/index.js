140e7cd94661510e42d6b22c54e34520c6417e7c
Miroslav Bajtoš <miroslav@strongloop.com>
2014-05-14 15:48:31 +0200

    acl: implement the DataSource generator
    Implement `yo loopback:acl` that calls Project methods to update
    the project files and add a new ACL entry for a given model.

---
+++ acl/index.js
@@ -0,0 +1,160 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+
+var actions = require('../lib/actions');
+var helpers = require('../lib/helpers');
+
+module.exports = yeoman.generators.Base.extend({
+  // NOTE(bajtos)
+  // This generator does not track file changes via yeoman,
+  // as loopback-workspace is editing (modifying) files when
+  // saving project changes.
+
+  loadProject: actions.loadProject,
+
+  loadModels: function() {
+    var done = this.async();
+    this.project.models(function(err, results) {
+      if (err) {
+        return done(err);
+      }
+      this.projectModels = results;
+      this.modelNames = results.map(function(m) {
+        return m.name;
+      });
+      done();
+    }.bind(this));
+  },
+
+  askForModel: function() {
+    var done = this.async();
+
+    var modelChoices =
+      [{ name: '(all existing models)', value: null }]
+      .concat(this.modelNames);
+
+    var prompts = [
+      {
+        name: 'model',
+        message: 'Select the model to apply the ACL entry to:',
+        type: 'list',
+        default: 0,
+        choices: modelChoices
+      }
+    ];
+
+    this.prompt(prompts, function(answers) {
+      this.modelName = answers.model;
+      if (this.modelName) {
+        this.modelDefinition = this.projectModels.filter(function(m) {
+          return m.name === answers.model;
+        })[0];
+      }
+      done();
+    }.bind(this));
+
+  },
+
+  askForParameters: function() {
+    var done = this.async();
+
+    var prompts = [
+      {
+        name: 'scope',
+        message: 'Select the ACL scope:',
+        type: 'list',
+        default: 'all',
+        choices: [
+          { name: 'All methods and properties', value: 'all' },
+          { name: 'A single method', value: 'method' },
+          { name: 'A single property', value: 'property' }
+        ]
+      },
+      {
+        name: 'property',
+        message: 'Enter the method name',
+        when: function(answers) {
+          return answers.scope === 'method';
+        }
+      },
+      {
+        name: 'property',
+        message: 'Enter the property name',
+        when: function(answers) {
+          return answers.scope === 'property';
+        }
+      },
+      {
+        name: 'accessType',
+        message: 'Select the access type:',
+        type: 'list',
+        default: 'all',
+        choices: [
+          { name: 'All (match all types)', value: 'all' },
+          'read',
+          'write',
+          'execute',
+        ]
+      },
+      {
+        name: 'role',
+        message: 'Select the role',
+        type: 'list',
+        default: 'everyone',
+        choices: [
+          { name: 'All users', value: 'everyone' },
+          { name: 'Any unauthenticated user', value: 'unauthenticated' },
+          { name: 'Any authenticated user', value: 'authenticated' },
+          { name: 'Any user related to the object', value: 'related' },
+          { name: 'The user owning the object', value: 'owner' },
+        ]
+      },
+      {
+        name: 'permission',
+        message: 'Select the permission to apply',
+        type: 'list',
+        choices: [
+          { name: 'Explicitly grant access to the model', value: 'allow' },
+          { name: 'Explicitly deny access to the model', value: 'deny' },
+          { name: 'Generate an alarm of the access to the model',
+            value: 'alarm' },
+          { name: 'Log the access to the model', value: 'audit' }
+        ]
+      }
+    ];
+    this.prompt(prompts, function(answers) {
+      this.aclOptions = {};
+      if (this.modelName) {
+        this.aclOptions.model = this.modelName;
+      } else {
+        this.aclOptions['all-models'] = true;
+      }
+
+      if (answers.property) {
+        this.aclOptions.property = answers.property;
+      }
+
+      var flags = ['scope', 'accessType', 'role', 'permission'];
+      flags.forEach(function(k) {
+        if (answers[k]) {
+          this.aclOptions[answers[k]] = true;
+        }
+      }.bind(this));
+      done();
+    }.bind(this));
+  },
+
+  acl: function() {
+    var done = this.async();
+    this.project.addPermission(this.aclOptions, function(err) {
+      if (!err) {
+        return this.project.saveToFiles(this.projectDir, done);
+      }
+
+      if (err.name === 'ValidationError') {
+        helpers.reportValidationError(err, this.log);
+      }
+      return done(err);
+    }.bind(this));
+  }
+});
---
+++ test/acl.test.js
@@ -0,0 +1,56 @@
+/*global describe, beforeEach, it */
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var Project = require('loopback-workspace').models.Project;
+var SANDBOX =  path.resolve(__dirname, 'sandbox');
+var fs = require('fs');
+var expect = require('must');
+
+describe('loopback:acl generator', function() {
+  beforeEach(function createSandbox(done) {
+    helpers.testDirectory(SANDBOX, done);
+  });
+
+  beforeEach(function createProject(done) {
+    Project.createFromTemplate(SANDBOX, 'test-app', 'mobile', done);
+  });
+
+  it('adds an entry to models.json', function(done) {
+    var aclGen = givenAclGenerator();
+    helpers.mockPrompt(aclGen, {
+      model: 'user',
+      scope: 'all',
+      accessType: 'all',
+      role: 'everyone',
+      permission: 'audit'
+    });
+
+    aclGen.run({}, function() {
+      var models = readModelsJsonSync();
+      var userOpts = models.user.options || {};
+      var userAcls = userOpts.acls;
+
+      expect(userAcls).to.eql([{
+        accessType: '*',
+        permission: 'AUDIT',
+        principalType: 'ROLE',
+        principalId: '$everyone'
+      }]);
+      done();
+    });
+  });
+
+  function givenAclGenerator() {
+    var deps = [ '../../acl' ];
+    var name = 'loopback:acl';
+    var gen = helpers.createGenerator(name, deps, [], {});
+    return gen;
+  }
+
+  function readModelsJsonSync() {
+    var filepath = path.resolve(SANDBOX, 'models.json');
+    var content = fs.readFileSync(filepath, 'utf-8');
+    return JSON.parse(content);
+  }
+});
