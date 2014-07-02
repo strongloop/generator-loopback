d40b06fa9010f147efec56d554e4d17f67cfb9e8
Miroslav Bajtoš <miroslav@strongloop.com>
2014-07-02 15:53:43 +0200

    app: Provide custom copyRecursive implementation
    Override `Workspace.copyRecursive` with yeoman's `directory` to enable
    conflict detection for templated files.

--- app/index.js
+++ app/index.js
@@ -21,20 +21,17 @@ module.exports = yeoman.generators.Base.extend({
     });
   },
 
-  injectProjectWriteFile: function() {
-    /* TODO inject `this.directory` to replace `ncp` in workspace
-    // Modify Project.writeFile use yeoman's write
-    var _projectWriteFile = Project.writeFile;
-    Project.writeFile = function(filepath, content, encoding, cb) {
-      this.write(filepath, content, { encoding: encoding });
-      cb();
+  injectWorkspaceCopyRecursive: function() {
+    var originalMethod = Workspace.copyRecursive;
+    Workspace.copyRecursive = function(src, dest, cb) {
+      this.directory(src, dest);
+      process.nextTick(cb);
     }.bind(this);
 
-    // Restore Project.writeFile when done
+    // Restore the original method when done
     this.on('end', function() {
-      Project.writeFile = _projectWriteFile;
+      Workspace.copyRecursive = originalMethod;
     });
-    */
   },
 
   initWorkspace: function() {
