310e3043da1484db50058f591525f94bec671e67
Raymond Feng <raymond@strongloop.com>
2014-10-14 14:07:00 -0700

    Add loopback:install generator

---
+++ install/index.js
@@ -0,0 +1,97 @@
+'use strict';
+var chalk = require('chalk');
+var yeoman = require('yeoman-generator');
+// var wsModels = require('loopback-workspace').models;
+
+var actions = require('../lib/actions');
+var helpers = require('../lib/helpers');
+var path = require('path');
+
+module.exports = yeoman.generators.Base.extend({
+  // NOTE(bajtos)
+  // This generator does not track file changes via yeoman,
+  // as loopback-workspace is editing (modifying) files when
+  // saving project changes.
+
+  constructor: function() {
+    yeoman.generators.Base.apply(this, arguments);
+
+    this.argument('name', {
+      desc: 'Name of the module to create.',
+      required: false,
+      type: String
+    });
+  },
+
+  help: function() {
+    return helpers.customHelp(this);
+  },
+
+  loadProject: actions.loadProject,
+
+  askForName: function() {
+    var done = this.async();
+
+    var prompts = [
+      {
+        name: 'name',
+        message: 'Enter the module name:',
+        default: this.name,
+        validate: function(name) {
+          if (!name) {
+            return 'Name must be provided';
+          } else {
+            return true;
+          }
+        }
+      }
+    ];
+
+    this.prompt(prompts, function(props) {
+      this.name = props.name;
+      done();
+    }.bind(this));
+
+  },
+
+  askForParameters: function() {
+    var done = this.async();
+
+    this.displayName = chalk.yellow(this.name);
+
+    var prompts = [
+      {
+        name: 'dev',
+        message: 'Is ' + this.displayName + ' a development dependency?',
+        default: false,
+        type: 'confirm'
+      }
+    ];
+
+    this.prompt(prompts, function(props) {
+      this.dev = props.dev;
+
+      done();
+    }.bind(this));
+  },
+
+  installDep: function() {
+    var self = this;
+    var done = this.async();
+    this.npmInstall([this.name], { 'saveDev': this.dev, 'save': !this.dev },
+      function(err) {
+        if (err) {
+          return done(err);
+        }
+        var pkgName = self.name.split('@')[0];
+        var pkg = self.dest.readJSON(
+          path.join('node_modules', pkgName, 'package.json'));
+        if (pkg && pkg['loopback-component']) {
+          console.log(pkg['loopback-component']);
+        }
+        done();
+      });
+  }
+});
+
+
---
+++ test/install.test.js
@@ -0,0 +1,89 @@
+/*global describe, beforeEach, afterEach, it */
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var wsModels = require('loopback-workspace').models;
+var SANDBOX =  path.resolve(__dirname, 'sandbox');
+var fs = require('fs');
+var expect = require('must');
+var common = require('./common');
+
+describe('loopback:install generator', function() {
+  beforeEach(function createSandbox(done) {
+    helpers.testDirectory(SANDBOX, done);
+  });
+
+  beforeEach(function createProject(done) {
+    common.createDummyProject(SANDBOX, 'test-app', done);
+  });
+
+  afterEach(common.resetWorkspace);
+
+  it('adds a dep to package.json', function(done) {
+    var instGen = givenInstallGenerator();
+    helpers.mockPrompt(instGen, {
+      name: 'debug',
+      dev: false
+    });
+
+    instGen.run({}, function() {
+      var pkg = readJsonSync('package.json');
+      expect(pkg.dependencies).to.have.property('debug');
+
+      pkg = readJsonSync('node_modules/debug/package.json');
+      expect(pkg.name).to.equal('debug');
+
+      done();
+    });
+  });
+
+  it('adds a dep with specific version to package.json', function(done) {
+    var instGen = givenInstallGenerator();
+    helpers.mockPrompt(instGen, {
+      name: 'debug@2.0.0',
+      dev: false
+    });
+
+    instGen.run({}, function() {
+      var pkg = readJsonSync('package.json');
+      expect(pkg.dependencies).to.have.property('debug');
+
+      pkg = readJsonSync('node_modules/debug/package.json');
+      expect(pkg.name).to.equal('debug');
+      expect(pkg.version).to.equal('2.0.0');
+      done();
+    });
+  });
+
+
+  it('adds a dev dep to package.json', function(done) {
+    var instGen = givenInstallGenerator();
+    helpers.mockPrompt(instGen, {
+      name: 'debug',
+      dev: true
+    });
+
+    instGen.run({}, function() {
+      var pkg = readJsonSync('package.json');
+      expect(pkg.devDependencies).to.have.property('debug');
+
+      pkg = readJsonSync('node_modules/debug/package.json');
+      expect(pkg.name).to.equal('debug');
+
+      done();
+    });
+  });
+
+  function givenInstallGenerator() {
+    var name = 'loopback:install';
+    var path = '../../install';
+    var gen = common.createGenerator(name, path);
+    return gen;
+  }
+
+  function readJsonSync(relativePath) {
+    var filepath = path.resolve(SANDBOX, relativePath);
+    var content = fs.readFileSync(filepath, 'utf-8');
+    return JSON.parse(content);
+  }
+});
