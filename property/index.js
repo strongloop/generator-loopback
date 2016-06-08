950c8b67e1af18532231e504f2d2e152dd52fad9
Alex Pitigoi <apitigoi@ca.ibm.com>
2016-06-08 17:31:32 -0400

    validate model property default value type

--- property/index.js
+++ property/index.js
@@ -12,6 +12,7 @@ var helpers = require('../lib/helpers');
 var validateRequiredName = helpers.validateRequiredName;
 var checkPropertyName = helpers.checkPropertyName;
 var typeChoices = helpers.getTypeChoices();
+var debug = require('debug')('loopback:generator:property');
 
 module.exports = yeoman.Base.extend({
   // NOTE(bajtos)
@@ -67,6 +68,7 @@ module.exports = yeoman.Base.extend({
         name: 'name',
         message: 'Enter the property name:',
         validate: checkPropertyName,
+        default: this.propDefinition && this.propDefinition.name,
         when: function() {
           return !this.name && this.name !== 0;
         }.bind(this)
@@ -75,6 +77,9 @@ module.exports = yeoman.Base.extend({
         name: 'type',
         message: 'Property type:',
         type: 'list',
+        default: this.propDefinition && 
+          (Array.isArray(this.propDefinition.type) ? 
+            'array' : this.propDefinition.type),
         choices: typeChoices
       },
       {
@@ -90,6 +95,10 @@ module.exports = yeoman.Base.extend({
         name: 'itemType',
         message: 'The type of array items:',
         type: 'list',
+        default: this.propDefinition && 
+          this.propDefinition.type &&
+          Array.isArray(this.propDefinition.type) &&
+          this.propDefinition.type[0],
         choices: typeChoices.filter(function(t) { return t !== 'array'; }),
         when: function(answers) {
           return answers.type === 'array';
@@ -112,10 +121,17 @@ module.exports = yeoman.Base.extend({
       {
          name: 'defaultValue',
          message: 'Default value[leave blank for none]:',
-         default: null
+         default: null,
+         when: function(answers) {
+          return answers.type !== null && 
+            answers.type !== 'buffer' &&
+            answers.type !== 'any' &&
+            typeChoices.indexOf(answers.type) !== -1;
+        }
       }
     ];
     return this.prompt(prompts).then(function(answers) {
+      debug('answers: %j', answers);
       this.name = answers.name || this.name;
       if (answers.type === 'array') {
         var itemType =  answers.customItemType || answers.itemType;
@@ -123,42 +139,134 @@ module.exports = yeoman.Base.extend({
       } else {
         this.type = answers.customType || answers.type;
       }
-      this.required = answers.required;
-      this.defaultValue = answers.defaultValue;
+
+      this.propDefinition = {
+        name: this.name,
+        type: this.type,
+      };
+      if (answers.required) {
+        this.propDefinition.required = Boolean(answers.required);
+      }
+
+      if (answers.defaultValue === '') {
+          this.log('Warning: please enter the ' + this.name +
+            ' property again. The default value provided "' +
+            answers.defaultValue + 
+            '" is not valid for type: ' + this.type);
+          return this.askForParameters();
+      }
+
+      try {
+        debug('property definition input: %j', this.propDefinition);
+        coerceDefaultValue(this.propDefinition, answers.defaultValue);
+        debug('property coercion output: %j', this.propDefinition);
+      } catch (err) {
+        debug('Failed to coerce property default value: ', err);
+        this.log('Warning: please enter the ' + this.name +
+          ' property again. The default value provided "' +
+          answers.defaultValue + 
+          '" is not valid for the selected type: ' + this.type);
+        return this.askForParameters();
+      }
     }.bind(this));
   },
 
   property: function() {
     var done = this.async();
-    var def = {
-      name: this.name,
-      type: this.type
-    };
-    if (this.required) {
-      def.required = true;
-    }
-    if (this.defaultValue) {
-      if (this.type === 'boolean'){
-        if (['true', '1', 't'].indexOf(this.defaultValue) !== -1 ){
-          def.default = true;
-        } else {
-          def.default = false;
-        }
-      } else if (this.defaultValue === 'uuid' || this.defaultValue === 'guid'){
-         def.defaultFn = this.defaultValue;
-      } else if ((this.type === 'date' || this.type === 'datetime') &&
-                  this.defaultValue.toLowerCase() === 'now'){
-         def.defaultFn = 'now';
-      } else {
-         def.default = this.defaultValue;
-      }
-    }
-
-    this.modelDefinition.properties.create(def, function(err) {
+    this.modelDefinition.properties.create(this.propDefinition, function(err) {
       helpers.reportValidationError(err, this.log);
       return done(err);
     }.bind(this));
   },
-
   saveProject: actions.saveProject
 });
+
+function coerceDefaultValue(propDef, value) {
+  var itemType;
+  if (Array.isArray(propDef.type)) {
+    itemType = propDef.type[0];
+    propDef.type = 'array';
+  }
+
+  switch (propDef.type) {
+    case 'string':
+      if (value === 'uuid' || value === 'guid'){
+        propDef.defaultFn = value;
+      } else {
+        propDef.default = value;
+      }
+      break;
+    case 'number':
+      propDef.default = castToNumber(value);
+      break;
+    case 'boolean':
+      propDef.default = castToBoolean(value);
+      break;
+    case 'date':
+      if (value.toLowerCase() === 'now'){
+        propDef.defaultFn = 'now';
+      } else {
+        propDef.default = castToDate(value);
+      }
+      break;
+    case 'array':
+      propDef.type = [itemType];
+      if (itemType === 'string') {
+        propDef.default = value.replace(/[\s,]+/g, ',').split(',');
+      } else if (itemType === 'number') {
+        propDef.default = value.replace(/[\s,]+/g, ',').split(',')
+          .map(castToNumber);
+      } else if (itemType === 'boolean') {
+        propDef.default = value.replace(/[\s,]+/g, ',').split(',')
+          .map(castToBoolean);
+      } else if (itemType === 'date') {
+        propDef.default = value.replace(/[\s,]+/g, ',').split(',')
+          .map(castToDate);
+      } else {
+        propDef.default = value;
+      }
+      break;
+    case 'geopoint':
+      if (value.indexOf('lat') !== -1 && value.indexOf('lng') !== -1) {
+        propDef.default = JSON.parse(value);
+      } else {
+        var geo = value.replace(/[\s,]+/g, ',').split(',');
+        propDef.default = {};
+        propDef.default.lat = castToNumber(geo[0]);
+        propDef.default.lng = castToNumber(geo[1]);
+      }
+      break;
+    case 'object':
+      propDef.default = JSON.parse(value);
+      break;
+    default:
+      propDef.default = value;
+  }
+}
+
+function castToDate(value) {
+  var dateValue;
+  var isNumber = /^[0-9]+$/.test(value);
+  if (isNumber) {
+    dateValue = new Date(castToNumber(value));
+  } else {
+    dateValue = new Date(value);
+  }
+  return dateValue;
+}
+
+function castToNumber(value) {
+  var numberValue = Number(value);
+  if (isNaN(numberValue)) {
+    throw Error('Invalid default number value: '+ value);
+  }
+  return numberValue;
+}
+
+function castToBoolean(value) {
+  if (['true', '1', 't', 'false', '0', 'f'].indexOf(value) === -1) {
+    throw Error('Invalid default boolean value "'+ value +
+      '". Expected default values: true|false, 1|0, t|f');
+  }
+  return (['true', '1', 't'].indexOf(value) !== -1) ? true : false;
+}
