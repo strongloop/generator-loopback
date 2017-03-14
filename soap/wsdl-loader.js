30a142e89549ed5953fd679e6047fa51267d06d6
rashmihunt <hrashmi@us.ibm.com>
2017-03-14 14:24:49 -0700

    create remote methods. models

--- soap/wsdl-loader.js
+++ soap/wsdl-loader.js
@@ -9,14 +9,13 @@ var chalk = require('chalk');
 
 var fs = require('fs');
 var async = require('async');
-
+var soapGenerator = require('loopback-soap');
 var soap = require('strong-soap').soap;
 var WSDL = soap.WSDL;
 var path = require('path');
 
-// loads remote WSDL or local WSDL using strong-soap module APIs
+// loads remote WSDL or local WSDL using strong-soap module APIs.
 function loadWsdl(wsdlUrl, log, cb) {
-  log(chalk.blue('Loading ' + wsdlUrl + '...'));
   WSDL.open(wsdlUrl, {}, function(err, wsdl) {
     if (err) {
       return cb(err);
@@ -24,19 +23,75 @@ function loadWsdl(wsdlUrl, log, cb) {
     cb(null, wsdl);
   });
 }
-// currently it just loads the WSDL and prints out services defined in the WSDL
-function generate(wsdlUrl, log, cb) {
+
+// get services defined in the wsdl
+exports.getServices = function getServices (wsdlUrl, log, cb) {
   loadWsdl(wsdlUrl, log, function(err, wsdl) {
     if (err) {
       return cb(err);
     }
-    var services = wsdl.services;
-    for (var s in services) {
-      var service = services[s];
-      log(chalk.blue('Service Name: ' + service.$name));
-    }
-    return cb(null, wsdl);
-  });
+    this.wsdl = wsdl;
+    this.wsdlUrl = wsdlUrl;
+    this.services = wsdl.services;
+    return cb(null,  this.services);
+  }.bind(this));
 }
 
-module.exports = generate;
+// get bindings for the service
+exports.getBindings = function getBindings (serviceName) {
+  this.selectedService =  this.services[serviceName];
+  var ports = this.selectedService.ports;
+  var bindingNames = [];
+  for(var name in ports) {
+    var binding = ports[name].binding;
+    bindingNames.push(binding.$name);
+  }
+  this.bindingNames = bindingNames;
+  return this.bindingNames;
+}.bind(this);
+
+// get operations for the binding
+exports.getOperations = function getOperations(bindingName) {
+  this.selectedBinding =  this.wsdl.definitions.bindings[bindingName];
+  var opNames = [];
+  for(var opName in this.selectedBinding.operations) {
+    opNames.push(opName);
+  }
+  return opNames;
+}.bind(this);
+
+// get operation objects for list of selected operation names
+function getSelectedOperations(selectedBinding, operationNames) {
+  var bindingOperations = selectedBinding.operations;
+  var operations = [];
+  var opNames = [];
+  for (var opName in operationNames) {
+    var name = operationNames[opName];
+    operations.push(bindingOperations[name]);
+  }
+  return operations;
+}
+
+// generate remote method and models for list of operations
+exports.generateAPICode  = function generateAPICode(operationNames) {
+  var apis = [];
+  var apiData = {
+    "wsdl": this.wsdl,
+    "wsdlUrl": this.wsdlUrl,
+    "service": this.selectedService.$name,
+    "binding": this.selectedBinding.$name,
+    "operations": getSelectedOperations(this.selectedBinding, operationNames)
+  }
+  var code = soapGenerator.generateRemoteMethods(apiData);
+  var models = soapGenerator.generateModels(apiData.wsdl, apiData.operations);
+  var api = {
+    code: code,
+    models: models
+  };
+  apis.push(api);
+  return apis;
+}.bind(this);
+
+
+
+
--- test/soap.test.js
+++ test/soap.test.js
@@ -20,18 +20,80 @@ describe('loopback:soap', function() {
   });
 
   beforeEach(function createProject(done) {
-    common.createDummyProject(SANDBOX, 'test-app', done);
+    common.createDummyProject(SANDBOX, 'test-soapapp', done);
   });
 
-  it('Load wsdl and print service/s name',
+  it('stock quote wsdl',
     function(done) {
       var modelGen = givenModelGenerator();
       helpers.mockPrompt(modelGen, {
         url: path.join(__dirname, 'soap/stockquote.wsdl'),
+        service: 'StockQuote',
+        binding: 'StockQuoteSoap',
+        operations: ['GetQuote']
       });
 
       // this runs command  loopback:soap command with mock up /test/soap/stockquote.wsdl as input from command prompt
       modelGen.run(function() {
+        var content = readModelJsonSync('get-quote');
+        expect(content).to.not.have.property('public');
+        expect(content).to.have.property('properties');
+        expect(content.properties.symbol.type).to.eql('string');
+        expect(content.properties.globals.type).to.eql('Globals');
+
+        var content = readModelJsonSync('globals');
+        expect(content).to.not.have.property('public');
+        expect(content).to.have.property('properties');
+        expect(content.properties.Promise.type).to.eql('boolean');
+        expect(content.properties.Promise.type).to.eql('boolean');
+
+        var content = readModelJsonSync('get-quote-response');
+        expect(content).to.not.have.property('public');
+        expect(content).to.have.property('properties');
+        expect(content.properties.GetQuoteResult.type).to.eql('string');
+
+        var modelConfig = readModelConfigSync('server');
+        expect(modelConfig).to.have.property('GetQuote');
+        expect(modelConfig.GetQuote).to.have.property('public', true);
+        done();
+      });
+    });
+
+  it('periodic table wsdl',
+    function(done) {
+      var modelGen = givenModelGenerator();
+      helpers.mockPrompt(modelGen, {
+        url: 'http://www.webservicex.net/periodictable.asmx?WSDL',
+        service: 'periodictable',
+        binding: 'periodictableSoap',
+        operations:[ 'GetAtomicNumber', 'GetAtomicWeight']
+      });
+
+      // this runs command  loopback:soap command with mock up /test/soap/stockquote.wsdl as input from command prompt
+      modelGen.run(function() {
+        var content = readModelJsonSync('get-atomic-weight');
+        expect(content).to.not.have.property('public');
+        expect(content).to.have.property('properties');
+        expect(content.properties.ElementName.type).to.eql('string');
+
+        var content = readModelJsonSync('get-atomic-weight-response');
+        expect(content.properties.GetAtomicWeightResult.type).to.eql('string');
+
+        var content = readModelJsonSync('get-atomic-number');
+        expect(content).to.not.have.property('public');
+        expect(content).to.have.property('properties');
+        expect(content.properties.ElementName.type).to.eql('string');
+
+        var content = readModelJsonSync('get-atomic-number-response');
+        expect(content.properties.GetAtomicNumberResult.type).to.eql('string');
+
+        var modelConfig = readModelConfigSync('server');
+        expect(modelConfig).to.have.property('GetAtomicWeight');
+        expect(modelConfig.GetAtomicWeight).to.have.property('public', true);
+
+        expect(modelConfig).to.have.property('GetAtomicNumber');
+        expect(modelConfig.GetAtomicNumber).to.have.property('public', true);
+
         done();
       });
     });
@@ -42,5 +104,18 @@ describe('loopback:soap', function() {
     var deps = [];
     var gen = common.createGenerator(name, path, deps, modelArgs, {});
     return gen;
+  }
+
+  function readModelJsonSync(name) {
+    var soapJson = path.resolve(SANDBOX, 'server/models/' + name + '.json');
+    expect(fs.existsSync(soapJson), 'file exists');
+    return JSON.parse(fs.readFileSync(soapJson));
+  }
+
+  function readModelConfigSync(facet) {
+    facet = facet || 'server';
+    var filepath = path.resolve(SANDBOX, facet, 'model-config.json');
+    var content = fs.readFileSync(filepath, 'utf-8');
+    return JSON.parse(content);
   }
 });
