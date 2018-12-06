strongloop/generator-loopback

commit 49a6d68dfcda2abe6bd8039815187cf2fed1a44d

fix model generator

property/index.js
@@ -23,6 +23,11 @@ module.exports = class PropertyGenerator extends ActionsMixin(yeoman) {
   // as loopback-workspace is editing (modifying) files when
   // saving project changes.
 
+  constructor(args, opts) {
+    super(args, opts);
+    this.modelEmitter = opts.modelEmitter;
+  }
+
   help() {
     return helpText.customHelp(this, 'loopback_property_usage.txt');
   }
@@ -196,6 +201,10 @@ module.exports = class PropertyGenerator extends ActionsMixin(yeoman) {
     debug('saving project...');
     this.saveProjectForGenerator();
     debug('saved project.');
+  }
+
+  emitEventEnd() {
+    if (this.modelEmitter) this.modelEmitter.emit('finished');
   }
 };
 

model/index.js
@@ -17,6 +17,7 @@ var helpText = require('../lib/help');
 var validateRequiredName = helpers.validateRequiredName;
 var validateOptionalName = helpers.validateOptionalName;
 var fs = require('fs');
+const EventEmitter = require('events');
 
 module.exports = class ModelGenerator extends ActionsMixin(yeoman) {
   // NOTE(bajtos)
@@ -354,31 +355,32 @@ module.exports = class ModelGenerator extends ActionsMixin(yeoman) {
           validate: validateOptionalName,
         },
       ];
-      return this.prompt(prompts).then(function(answers) {
+      this.prompt(prompts).then(function(answers) {
         if (answers.propertyName == null || answers.propertyName === '') {
           return done();
         }
-
-        this.invoke(
+        const emitter = new EventEmitter();
+        this.composeWith(
           'loopback:property',
           {
-            options: {
-              nested: true,
-              projectDir: this.projectDir,
-              project: this.project,
-              modelName: this.name,
-              propertyName: answers.propertyName,
-            },
-          },
-          function(err) {
-            if (err) {
-              return done(err);
-            }
-            this.log(g.f('\nLet\'s add another %s property.',
-              this.displayName));
-            this.property();
-          }.bind(this)
+            nested: true,
+            projectDir: this.projectDir,
+            project: this.project,
+            modelName: this.name,
+            propertyName: answers.propertyName,
+            modelEmitter: emitter,
+          }
         );
+        // `this.composeWith` doesn't wait until the sub-generator finishes,
+        // but runs tasks according to yeoman-generator's run loop, which determine
+        // the task sequences by phases, details see
+        // http://yeoman.io/authoring/composability.html
+        // as a workaround, an event emitter is introduced to invoke another property prompts
+        emitter.on('finished', ()=> {
+          this.log(g.f('\nLet\'s add another %s property.', this.displayName));
+          this.property();
+        });
+        return done();
       }.bind(this));
     }
   }

.npmignore
@@ -1,0 +1,32 @@
+.idea
+
+# Logs
+logs
+*.log
+
+# Runtime data
+pids
+*.pid
+*.seed
+
+# Directory for instrumented libs generated by jscoverage/JSCover
+lib-cov
+
+# Coverage directory used by tools like istanbul
+coverage
+
+# Grunt intermediate storage
+.grunt
+
+# Compiled binary addons (http://nodejs.org/api/addons.html)
+build/Release
+
+# Dependency directory
+node_modules/
+
+temp/
+test/sandbox
+test/.pkgcache/
+.pkgcache/
+test
+.travis.yml

commit 134bb854f4fec04a11689d23590fe628a6e119ca

Fix build failure introduced by 49a6d68dfc
Modify PropertyGenerator to handle the case where no options object
was provided (typically in our unit tests).

property/index.js
@@ -25,7 +25,7 @@ module.exports = class PropertyGenerator extends ActionsMixin(yeoman) {
 
   constructor(args, opts) {
     super(args, opts);
-    this.modelEmitter = opts.modelEmitter;
+    this.modelEmitter = opts && opts.modelEmitter;
   }
 
   help() {